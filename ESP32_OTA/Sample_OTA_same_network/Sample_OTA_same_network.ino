d37f5d659dc7237d6d32f5122fe55f6c9f260c99
Ramu-acharya <156974017+Ramu-acharya@users.noreply.github.com>
2024-03-21 22:40:54 +0545

    Code for OTA support with port info

---
+++ ESP32_OTA/Sample_OTA_same_network/Sample_OTA_same_network.ino
@@ -0,0 +1,33 @@
+#include <WiFi.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "khadgisujan"; // Replace with your WiFi SSID
+const char* password = "CLB279EF91"; // Replace with your WiFi password
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  // Start OTA
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle(); // Handle OTA updates
+
+  digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on
+  delay(500);                      // Wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // Turn the LED off
+  delay(300);                      // Wait for a second
+}
